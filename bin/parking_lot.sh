488b8bffde9054be35d1ed76c7ae1216f083a728
Zooperman <joy.calcutta@gmail.com>
2018-07-22 19:12:49 +0530

    added bin files

---
+++ bin/parking_lot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [$# -eq 0]
+then
+	python src/main/python/command_parser.py
+else
+	python src/main/python/command_parser.py $1
+fi
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
+pip install -U pip
 pip install pipenv
 pipenv install --system
 pyb -v
